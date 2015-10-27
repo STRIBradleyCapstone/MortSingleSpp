78add13e53fbb48b20f653da5dc59d3651ecf033
ashular <ashular@mail.bradley.edu>
2015-10-27 18:24:09 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,7 +8,8 @@ shinyUI(fluidPage(
     sidebarPanel(
       #Species select needs to be reimplemented.  Right now it acquires a list of species from mort.data AFTER calculation.  We need a list from the start.
       #To do this, I think that we can just create a list of species by cycling through one of the surveys.
-      #selectInput("speciesSelect", label = "Please enter a species to graph", choices = rownames(mort.data()$N), selected = rownames(mort.data()$N)[1]),
+      capture.output(allspp <- sort(unique(bci.full1$sp))),
+      selectInput("speciesSelect", label = "Please enter a species to graph", choices = allspp, selected = allspp[1]),
       numericInput("dbhNumSelect", label = "Amount of dbh Categories", value = 3, min = 1, max = 10),
       uiOutput("categoryNumerics"),
       actionButton("action", label = "Apply Changes"),
@@ -17,9 +18,13 @@ shinyUI(fluidPage(
     
     mainPanel(
       
-      actionButton("speciesAction", label = "Apply Species"),
       br(),h3(textOutput("speciesNameTextOut"), style = "position:absolute;left:300px"),
       plotOutput("speciesGraph"),
+      checkboxGroupInput("surveyPeriodCheckGroup", label = "Choose which survey periods to chart", 
+                         choices = c("1->2", "1->3", "1->4", "1->5", "1->6", "1->7", "2->3", "2->4", "2->5", "2->6", "2->7", 
+                                     "3->4", "3->5", "3->6", "3->7", "4->5", "4->6", "4->7", "5->6", "5->7", "6->7"), 
+                         selected = c("1->2", "1->3", "1->4", "1->5", "1->6", "1->7", "2->3", "2->4", "2->5", "2->6", "2->7", 
+                                      "3->4", "3->5", "3->6", "3->7", "4->5", "4->6", "4->7", "5->6", "5->7", "6->7"), inline = T),
       numericInput("yNum", label = "Y Axis", value = 0.2, min = 0.005, max = 1, step = 0.05),
       uiOutput("xNumeric")
     )

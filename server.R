ea05b4cd6e3a2701ea60b0e26522eafd22fe2f10
ashular <ashular@mail.bradley.edu>
2015-11-23 18:08:27 -0600

    Update server.R

--- server.R
+++ server.R
@@ -11,9 +11,18 @@ load("Data/full/bci.full6.rdata")
 load("Data/full/bci.full7.rdata")
 load("Data/Species/bci.spptable.rdata")
 
-CENSUS_COUNT <- 7
-
 shinyServer(function(input, output, session) {
+  
+  CENSUS_COUNT <- 7
+  graphMatrix = matrix("N", nrow = CENSUS_COUNT - 1, ncol = CENSUS_COUNT - 1)
+  colMatrix = matrix(c("#8b0000", 0, 0, 0, 0, 0,
+                       "#8b2323", "#8b4500", 0, 0, 0, 0,
+                       "#a52a2a", "#cd6600", "#8b6508", 0, 0, 0,
+                       "#CD3333", "#ee7600", "#cd950c", "#556b2f", 0, 0,
+                       "#ee3b3b", "#ff7f00", "#eead0e", "#6e8b3d", "#104e8b", 0,
+                       "#ff4040", "#ff8c00", "#ffb90f", "#bcee68", "#1c86ee", "#8968cd"), 
+                     nrow = CENSUS_COUNT - 1, ncol = CENSUS_COUNT - 1)
+  
   output$categoryNumerics <- renderUI({
     amount = input$dbhNumSelect
     validate(need(amount > 1, "Amount of dbh categories must be > 1"))
@@ -56,7 +65,7 @@ shinyServer(function(input, output, session) {
     input$speciesSelect
   })
   
-  survey1 <- eventReactive(input$action,{
+  survey1 <- reactive({
     switch(input$survey1Select, 
            "full1" = {return(1)},
            "full2" = {return(2)},
@@ -66,15 +75,13 @@ shinyServer(function(input, output, session) {
            "full6" = {return(6)})
   })
   
-  survey2 <- eventReactive(input$plotAction,{
-    slist = c("full1", "full2", "full3", "full4", "full5", "full6", "full7")
+  survey2 <- reactive({
+    slist <- c("full1", "full2", "full3", "full4", "full5", "full6", "full7")
     #match function calculates the indices of each survey input within a list of all surveys
     #Used to ensure that survey1 is less than survey2
-    cat("survey2Validate\n")
     validate(
       need(match(input$survey1Select, slist) < match(input$survey2Select, slist), "First survey must have occurred before the second survey")
     )
-    cat("survey2ValidateEnd\n")
     #returns the census number -1 so that using it to subset mort.dataList will yield the correct subset.
     switch(input$survey2Select,
            "full2" = {return(1)},
@@ -85,10 +92,9 @@ shinyServer(function(input, output, session) {
            "full7" = {return(6)})
   })
   
-  mort.data <- eventReactive(input$plotAction,{
-    cat("mort.data\n")
-    mort.dataList = matrix(list(), nrow = CENSUS_COUNT - 1, ncol = CENSUS_COUNT)
-    cat("buildingCensusList\n")
+  mort.data <- eventReactive(input$action,{
+    graphMatrix <<- matrix("N", nrow = CENSUS_COUNT - 1, ncol = CENSUS_COUNT - 1)
+    mort.dataList = matrix(list(), nrow = CENSUS_COUNT - 1, ncol = CENSUS_COUNT - 1)
     censusList = c(list(bci.full1[bci.full1$sp == speciesName(), 1:20]),
                    list(bci.full2[bci.full2$sp == speciesName(), 1:20]),
                    list(bci.full3[bci.full3$sp == speciesName(), 1:20]),
@@ -96,25 +102,39 @@ shinyServer(function(input, output, session) {
                    list(bci.full5[bci.full5$sp == speciesName(), 1:20]),
                    list(bci.full6[bci.full6$sp == speciesName(), 1:20]),
                    list(bci.full7[bci.full7$sp == speciesName(), 1:20]))
-    cat("censusListBuilt\n")
     for(i in 1:(CENSUS_COUNT - 1)){
       for(j in (i+1):CENSUS_COUNT){
         force(i)
         force(j)
         tempList = mortality.eachspp(censusList[[i]], censusList[[j]], classbreak = classes())
-        #templist["active"] = "N"
         mort.dataList[[i,j - 1]] <- list(tempList)
       }
       
     }
-    cat("MortDataEnd\n")
     return(mort.dataList)
     
   })
   
-  #observeEvent(input$plotAction, {
-  #  mort.data()[[survey1(), survey2()]]$active <- "Y"
-  #})
+  observeEvent(input$plotAction, {
+    force(survey1())
+    force(survey2())
+    if(!is.null(mort.data()))
+    {
+      graphMatrix[[survey1(), survey2()]] <<- "Y"
+    }
+  })
+  
+  observeEvent(input$clearAction, {
+    graphMatrix <<- matrix("N", nrow = CENSUS_COUNT - 1, ncol = CENSUS_COUNT)
+  })
+  
+  observeEvent(input$plotAllAction, {
+    graphMatrix <<- matrix("N", nrow = CENSUS_COUNT - 1, ncol = CENSUS_COUNT)
+    for(i in 1:CENSUS_COUNT)
+    {
+      matrix[[i,i+1]] <- "Y"
+    }
+  })
   
   dbhCatNum <- eventReactive(input$action, {
     input$dbhNumSelect
@@ -122,41 +142,37 @@ shinyServer(function(input, output, session) {
   
   output$speciesNameTextOut <- renderText({speciesName()})
   
-  #Graph rendering function must be reimplemented
   output$speciesGraph <- renderPlot({
-    cat("plot\n")
+    input$plotAction
+    input$clearAction
+    input$plotAllAction
     plot(x=NULL,y=NULL,type='l',xlim=c(0, input$xNum),ylim=c(0, input$yNum),ylab=paste(speciesName(),'mortality'),xlab='dbh')
     if(!is.null(mort.data()) && !is.null(survey2()))
     {
-      cat("dataNotNull\n")
-      mortdbh.graph(survey1(), survey2(),sp=speciesName(),xrange=c(0, input$xNum),yrange=c(0,input$yNum))
-    }
-    else
-    {
-      cat("dataNull\n")
-    }
-    cat("plotEnd\n")
-  })
-  
-  output$table <- renderTable({
-    if(!is.null(mort.data()))
-    {
-      assemble.demography(mort.data()[1,2],type="m",whichdbhcat=num)
+      for(i in 1:(CENSUS_COUNT-1)){
+        for(j in i:(CENSUS_COUNT-1)){
+          force(i)
+          force(j)
+          print(graphMatrix[[i,j]])
+          if(graphMatrix[[i,j]] == "Y")
+          {
+            mortdbh.graph(i, j,sp=speciesName(),xrange=c(0, input$xNum),yrange=c(0,input$yNum))
+          }
+        }
+      }
+      
     }
   })
   
-  #Species graphing function must be reimplemented
   mortdbh.graph=function(census1, census2 ,sp,xrange=NULL,yrange=NULL)
   {
-    cat("graphStart\n")
+    par(col = colMatrix[[census1, census2]])
     data = mort.data()[[census1, census2]]
     y=data[[1]]$rate
     aproblem=is.infinite(y)
     if(is.null(yrange)) yrange=c(0,max(y[!aproblem],na.rm=TRUE))
     if(is.null(xrange)) xrange=c(0,max(data[[1]]$dbhmean,na.rm=TRUE))
-    cat("graphing...\n")
-    lines(data[[1]]$dbhmean[!aproblem],y[!aproblem],type='l')
-    cat("graphEnd\n")
+    lines(data[[1]]$dbhmean[!aproblem],y[!aproblem],type='l',lwd=2)
   }
   
   

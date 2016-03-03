35131baa9ab00f263e8123623794118d846cbc9c
ashular <ashular@mail.bradley.edu>
2016-03-03 12:58:07 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,15 +1,25 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Mortality"),
+  titlePanel("Mortality & Growth"),
   
   sidebarLayout(
     
     sidebarPanel(
+      uiOutput("regionSelect"),
+      actionButton("regionAction", label="Load Region", inline = TRUE),
+      uiOutput("loadNotification", inline = TRUE),
       uiOutput("speciesSelect"),
-      numericInput("dbhNumSelect", label = "Amount of dbh Categories", value = 3, min = 2, max = 10),
-      uiOutput("categoryNumerics"),
-      actionButton("action", label = "Apply Changes"),
+      numericInput("dbhNumSelect", label = "Amount of dbh Categories", value = 3, min = 2, max = 100),
+      div(style = "display:inline-block; width:47%; float:left", 
+          numericInput("dbhBaseNumeric", label = "dbh Base", value = 10, min = 0, max = 10000)),
+      div(style = "display:inline-block; width:47%; float:right", 
+          numericInput("dbhGapNumeric", label = "dbh Gap", value = 10, min = 1, max = 10000)),
+      actionButton("dbhAction", label = "Apply dbh Options"),
+      wellPanel(id = "categoryWell", style = "overflow-y:scroll; max-height: 400px", 
+                uiOutput("categoryNumerics")),
+      radioButtons("calcTypeRadio", label = "Select a function", choices = c("Mortality", "Growth"), selected = "Mortality", inline = TRUE),
+      actionButton("action", label = "Calculate"),
       width = 3
     ),
     
@@ -17,13 +27,12 @@ shinyUI(fluidPage(
       
       br(),h3(textOutput("speciesNameTextOut"), style = "position:absolute;left:300px"),
       plotOutput("speciesGraph"),
-      selectInput("survey1Select", label = "Please select the first survey", choices = c("full1", "full2", "full3", "full4", "full5", "full6"), selected = "full1"),
-      selectInput("survey2Select", label = "Please select the second survey", choices = c("full2", "full3", "full4", "full5", "full6", "full7"), selected = "full2"),
-      numericInput("yNum", label = "Y Axis", value = 0.2, min = 0.005, max = 1, step = 0.05),
+      uiOutput("surveySlider"),
+      uiOutput("yNumeric"),
       uiOutput("xNumeric"),
       actionButton("plotAction", label = "Plot surveys"),
       actionButton("clearAction", label = "Clear graph"),
-      actionButton("plotAllAction", label = "Plot all surveys")
+      actionButton("plotAllAction", label = "Plot all consecutive surveys")
     )
   )
 ))
